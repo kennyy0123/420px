266c72a570260a8cebdf49ea16aef62123e78a84
Kevin Louzoun <louzou_k@epita.fr>
2017-04-30 00:32:33 +0200

    Add upload file

--- gallerie.php
+++ gallerie.php
@@ -1,5 +1,22 @@
 <?php
   session_start();
+
+  if( isset($_POST['upload'])) 
+  {
+    $content_dir = 'picture/'; 
+    $tmp_file = $_FILES['fichier']['tmp_name'];
+
+    if(!is_uploaded_file($tmp_file) )
+        exit("Le fichier n'existe pas");
+
+    $type_file = $_FILES['fichier']['type'];
+    $name_file = $_FILES['fichier']['name'];
+
+    if(!move_uploaded_file($tmp_file, $content_dir . $name_file) )
+        exit("Le fichier est impossible à créer : $content_dir");
+  }
+
+  
 ?>
 
 <html lang="fr">
@@ -30,5 +47,16 @@
     <?php endif; ?>
   </div>
 </nav>
+<?php if (isset($_SESSION['pseudo'])) : ?>
+
+  <form method="post" enctype="multipart/form-data">
+  <div id="upload_picture">
+    <input type="file" class="custom-file-upload" name="fichier" size="30" accept="image/x-png,image/gif,image/jpeg">
+    <input class="button is-small" type="submit" name="upload" value="Uploader">
+  </div>
+  </form>
+<?php endif; ?>
+</div>
+
 </body>
 </html>
